c6ff718e174838a434fb886ccb853bafe0b74288
Mechamorph <kazam0180@proton.me>
2026-08-21 20:55:46 +0500

    replace kazam0180.github.io url

--- .github/scripts/select-apps-missing-screenshots.py
+++ .github/scripts/select-apps-missing-screenshots.py
@@ -17,7 +17,7 @@ APPS_DIR = sys.argv[1] if len(sys.argv) > 1 else "apps"
 
 blacklist = set()
 
-with urlopen("https://kazam0180.github.io/pla-site-testing/categories/command-line.json") as resp:
+with urlopen("https://portable-linux-apps.github.io/categories/command-line.json") as resp:
     data = json.loads(resp.read())
     for k in data.keys():
         blacklist.add(k)
